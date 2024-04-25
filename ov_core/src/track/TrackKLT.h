15644ffff47a240d3a43ee77b48dd602f14c8f33
izenderi <ziliang1121@gmail.com>
2024-04-25 12:20:16 -0700

    30 winsize

--- ov_core/src/track/TrackKLT.h
+++ ov_core/src/track/TrackKLT.h
@@ -146,7 +146,7 @@ namespace ov_core {
         // <RTEN> Change the win_size as the patch size for KLT tracking
         // How many pyramid levels to track on and the window size to reduce by
         int pyr_levels = 3;
-        cv::Size win_size = cv::Size(50, 50);
+        cv::Size win_size = cv::Size(30, 30);
         // </RTEN>
 
         // Last set of image pyramids
